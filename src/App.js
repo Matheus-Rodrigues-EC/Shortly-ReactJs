ed0b3a9dd1f9edde5416b2591c09ea6ebc47478d
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-05-21 15:49:16 -0300

    Configuration initial of project, remove unused files, and update files, for use on project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomeLogged from "./pages/HomeLogged";
+import HomeUnlogged from "./pages/HomeUnlogged";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Ranking from "./pages/Ranking";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+      <Routes>
+          <Route path="/" element={<HomeUnlogged />} />
+          <Route path="/home" element={<HomeLogged />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/ranking" element={<Ranking />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
