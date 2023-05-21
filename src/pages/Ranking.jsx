23ea153e2e992d31468a6ab38f941d6e69d56a97
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-05-21 17:21:21 -0300

    Create Ranking.jsx

---
+++ src/pages/Ranking.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export default function Ranking(){
+    
+    return (
+        <Container>
+            <h1>Ranking Page</h1>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    background-color: #FFFFFF;
+    margin: 0;
+    padding: 0;
+`
