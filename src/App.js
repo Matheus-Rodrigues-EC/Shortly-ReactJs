869cccb9b0d0d813c7248e827a2465864a0e6126
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-05-22 00:01:37 -0300

    Reorganize App functions and routers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 import HomeLogged from "./pages/HomeLogged";
 import HomeUnlogged from "./pages/HomeUnlogged";
@@ -7,15 +8,18 @@ import SignIn from "./pages/SignIn";
 import Ranking from "./pages/Ranking";
 
 function App() {
+
+  const [name, setName] = useState();
+
   return (
     <BrowserRouter>
 
       <Routes>
           <Route path="/" element={<HomeUnlogged />} />
-          <Route path="/home" element={<HomeLogged />} />
+          <Route path="/home" element={<HomeLogged name={name} setName={setName} />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
-          <Route path="/ranking" element={<Ranking />} />
+          <Route path="/ranking" element={<Ranking name={name}/>} />
 
       </Routes>
     </BrowserRouter>
