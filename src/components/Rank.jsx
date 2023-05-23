7702b8ecb22454dc88500c48e8efa86c14d3b89d
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-05-23 17:32:33 -0300

    Update Rank.jsx

--- src/components/Rank.jsx
+++ src/components/Rank.jsx
@@ -13,7 +13,7 @@ export default function Rank(){
                 setRank(res.data);
             })
             .catch((error) => {
-                alert(error.response.data)
+                alert(error)
             })
     }, [api, rank]);
 
