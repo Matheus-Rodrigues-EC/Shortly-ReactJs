0a2cd4dd763740f427331f74d1692794b78abfb0
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-05-23 11:46:57 -0300

    Fix error of open shorted link

--- src/components/ListLinks.jsx
+++ src/components/ListLinks.jsx
@@ -26,7 +26,7 @@ export default function ListLinks(props){
 
     function followLink(shortUrl){
         api.get(`${process.env.REACT_APP_API_URL}/urls/open/${shortUrl}`)
-            .then((res) => {})
+            .then((res) => {window.open(res.data)})
             .catch((error) => {alert(error.message)})
     }
 
